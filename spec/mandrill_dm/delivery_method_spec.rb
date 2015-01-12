368d3d993d7fb11b83cd9295882bd04c4deba1a8
John Dell <spovich@gmail.com>
2015-01-11 16:31:20 -0800

    Update `MandrillDm::DeliveryMethod` to capture the response from the Mandrill API and make it available.
    Example usage would be something like:
    
    ```
    message  = FooMailer.build(email).deliver_now
    response = message.delivery_method.response
    ```
    
    The response json will appear as indicated as here:
    https://mandrillapp.com/api/docs/messages.html
    
    Update the specs for `MandrillDm::DeliveryMethod` to better
    test the class behavior. Also, split tests into class tests and 
    integration tests.  These tests are borrowed (and only
    slightly modified) from the MYOB-Technology fork of mandrill_dm.

--- spec/mandrill_dm/delivery_method_spec.rb
+++ spec/mandrill_dm/delivery_method_spec.rb
@@ -1,39 +1,58 @@
 require 'spec_helper'
 
 describe MandrillDm::DeliveryMethod do
-  before(:each) do
-    Mail.defaults do
-      delivery_method MandrillDm::DeliveryMethod
+  let(:options)         { { 'some_option_key' => 'some option value' } }
+  let(:delivery_method) { MandrillDm::DeliveryMethod.new(options) }
+
+  context '#initialize' do
+    it 'sets passed options to `settings` attr_accesor' do
+      expect(delivery_method.settings).to eql(options)
     end
   end
 
-  context 'api' do
+  context '#deliver!' do
+    let(:mail_message) { instance_double(Mail::Message) }
+    let(:api_key)      { '1234567890' }
+    let(:dm_message)   { instance_double(MandrillDm::Message) }
+    let(:response)     { { 'some_response_key' => 'some response value' } }
+    let(:messages)     { instance_double(Mandrill::Messages, send: response) }
+    let(:api)          { instance_double(Mandrill::API, messages: messages) }
+
     before(:each) do
-      @api = double('Mandrill::API')
-      @api.stub_chain(:messages, :send).and_return({})
+      allow(Mandrill::API).to receive(:new).and_return(api)
+      allow(MandrillDm).to receive_message_chain(:configuration, :api_key).and_return(api_key)
+      allow(MandrillDm::Message).to receive(:new).and_return(dm_message)
     end
 
-    it 'gets instantiated' do
-      Mandrill::API.should receive(:new).with('1234567890').and_return(@api)
-      Mail.deliver do
-        from 'John Doe <name@domain.tld>'
-        body 'Hello world!'
-        subject 'A Test'
-        to 'name@domain.tld'
-        bcc 'name@domain.tld'
-      end
+    subject { delivery_method.deliver!(mail_message) }
+
+    it 'instantiates the Mandrill API with the configured API key' do
+      expect(Mandrill::API).to receive(:new).with(api_key).and_return(api)
+
+      subject
     end
 
-    it 'sends a message' do
-      expect {
-        Mail.deliver do
-          from 'John Doe <name@domain.tld>'
-          body 'Hello world!'
-          subject 'A Test'
-          to 'name@domain.tld'
-          bcc 'name@domain.tld'
-        end
-      }.not_to raise_error
+    it 'creates a Mandrill message from the Mail message' do
+      expect(MandrillDm::Message).to receive(:new).with(mail_message).and_return(dm_message)
+
+      subject
+    end
+
+    it 'sends the JSON version of the Mandrill message via the API' do
+      allow(dm_message).to receive(:to_json).and_return('Some message JSON')
+      expect(messages).to receive(:send).with('Some message JSON')
+
+      subject
+    end
+
+    it 'returns the response from sending the message' do
+      expect(subject).to eql(response)
+    end
+
+    it 'establishes the response for subsequent use' do
+      subject
+
+      expect(delivery_method.response).to eql(response)
     end
   end
 end

ce9b5f6980ca55e82627245c1e3be37cbaa64e07
John Dell <spovich@gmail.com>
2015-02-24 13:12:37 -0800

    Add Rakefile tasks.  Run `rake -T` to see new rake options. Add ‘rake’ and ‘simplecov’ as dev dependencies.
     * Add ‘travis-lint’ development-dependency
     * Add `bundler/gem_tasks` for gem releases (adds rake tasks)
     * Add `rake coverage` - run specs/simplecov
     * Add `rake spec` - run specs (like `rspec`)
     * Add `rake validate` - validate travis-ci config
     * remove deprecated config value (defaults to true)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,18 +1,28 @@
+require 'simplecov'
+SimpleCov.start do
+  coverage_dir 'tmp/coverage'
+  add_filter '/spec/'
+
+  SimpleCov.at_exit do
+    SimpleCov.result.format!
+    system('open tmp/coverage/index.html') if RUBY_PLATFORM['darwin']
+  end
+end if ENV['COVERAGE']
+
 require 'rubygems'
 require 'bundler/setup'
 Bundler.require(:default)
 
-require "mail"
+require 'mail'
 
 MandrillDm.configure do |config|
   config.api_key = '1234567890'
 end
 
 RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
 
-  config.before(:all) do 
+  config.before(:all) do
     Excon.defaults[:mock] = true
     Excon.stub({}, {body: '{}', status: 200}) # stubs any request to return an empty JSON string
   end

89e03f1d4392b2b52ff2d6b2d4d751675d0c5f15
Genaro Madrid <genaro@appio.mx>
2016-01-20 19:15:30 -0600

    send with send_template if a the template is present

--- lib/mandrill_dm/delivery_method.rb
+++ lib/mandrill_dm/delivery_method.rb
@@ -8,8 +8,30 @@ module MandrillDm
 
     def deliver!(mail)
       mandrill_api = Mandrill::API.new(MandrillDm.configuration.api_key)
-      message = Message.new(mail)
-      @response = mandrill_api.messages.send(message.to_json)
+      @message = Message.new(mail)
+      if @message.template
+        @response = mandrill_api.messages.send_template(@message.template, template_content, message_json)
+      else
+        @response = mandrill_api.messages.send(message_json)
+      end
     end
+
+    private
+
+      def message_json
+        @message_json ||= @message.to_json
+      end
+
+      def template_content
+        template_cnt = []
+        template_cnt_hash = eval(@message.template_content)
+        template_cnt_hash.each do |name, content|
+          template_cnt.push({
+            name: name.to_s,
+            content: content.is_a?(Symbol) ? message_json[content] : content
+          })
+        end
+        template_cnt
+      end
   end
 end
